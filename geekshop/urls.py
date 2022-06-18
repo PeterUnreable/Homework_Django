7ced2a2008e82a98fe0d05f54b23727b736a0db9
Peter_2021_Python <panchenko.pg@yandex.ru>
2022-06-18 13:33:48 +0300

    lesson3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,15 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+
 
 
 urlpatterns = [
     path('', mainapp.index ,name='index'),
     #path('', mainapp.main,name='main'),
+    path('products/', include('mainapp.urls', namespace='products')),
     path('products/', mainapp.products,name='products'),
     path('contacts/', mainapp.contacts,name='contacts'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def products(request, pk=None):
+    print(pk)
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
+from .models import ProductCategory, Product
+
 
 # Create your views here.
 
+def main(request):
+    title = 'главная'
+    products = Product.objects.all()[:4]
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', content)
 
 def index(request):
+    products = Product.objects.all[:2]
     list_params = {'no': 'Vau,',
                    'p1': True,
                     'info': False
                    };
 
-    context = render(request, 'mainapp/index.html', context = list_params)
-    return context
+    #context = render(request, 'mainapp/index.html', context = list_params)
+
+    context = {
+        'title' : 'главная',
+        'product' : 'products',
+    }
+
+    return render(request,'mainapp/index.html', context)
+    #return context
+    #context = render(request, 'mainapp/index.html', context=list_params)
+
+
 
 #def main(request):
 #    return render(request, 'mainapp/index.html')
@@ -26,7 +44,9 @@ def products(request):
     ]
 
     context =  {
-        'links_menu' : links_menu
+        'title' : 'каталог',
+        'links_menu' : links_menu,
+        'object' : 'Product.objects.get(id = 1)'
     }
 
 
